1c7721365e58cebfa9e53eafd7d4f810e61a424e
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-25 20:46:12 +0530

    Update print-the-greatest-number-in-the-list.py

--- print-the-greatest-number-in-the-list.py
+++ print-the-greatest-number-in-the-list.py
@@ -1,4 +1,14 @@
 #using sort() method
-li_1=[1,5,0,9,2,3]
-li_1.sort()
-print(li_1[-1])
+li=[1,5,0,9,2,3]
+li.sort()
+print(li[-1])
+
+#using for loop and if condition
+li=[1,5,0,9,2,3]
+x=li[0]
+for y in li:
+  if y > x:
+    x=y
+print(x)
+
+
