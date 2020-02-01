02e82f607ea901432151141aa96be0eb114e93f4
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-02-01 23:15:31 +0530

    Create number-divisible-by-3-or-7.py

---
+++ number-divisible-by-3-or-7.py
@@ -0,0 +1,9 @@
+#To check if number is divisible by 3 or 7
+
+num=int(input("Enter a number"))
+if num%3==0:
+	print("Number is divisible by 3")
+elif num%7==0:
+	print("Number is divisible by 7") 
+else:
+  print("Number is neither divisible by 3 nor 7")
