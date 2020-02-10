057a2093f07625c304161bc9d7fb9f5d406954b7
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-02-10 19:32:52 +0530

    Create operators-basic.py

---
+++ operators-basic.py
@@ -0,0 +1,40 @@
+Operators: Used in loops
+
+x=2 y=3
+
+#Arithmetic operators
+x+y
+x-y
+x*y
+x/y
+
+#Assignment operators
+x=x+2  // x+=2
+x=x-2  // x-=2
+x=x*2  // x*=2
+x=x/2  // x/=2
+x,y = 2,3 //as
+
+#Logical operators
+and
+or
+not
+x<3 and y<4 --> True
+x<3 and y<2 --> False
+x<3 or y<2 --> True
+x<3 or y<4 --> True
+x=True
+not x --> False
+
+#Relational operators
+a<b (check if a is less than to b)
+a>b (check if a is greater than b)
+a==b (check if a is equal to b)
+a<=b (check if a is less than equalt to b)
+a>=b (check if a is greater than equalt to b)
+a!=b (check if a is not equalt to b)
+
+#Unary opeators
+n=5
+-n will result into -5
+
