d6c4478086dbf8af3e41ea670e5a75d5db5f10de
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-02-11 10:38:33 +0530

    Update for-loop.py

--- for-loop.py
+++ for-loop.py
@@ -1,2 +1,21 @@
+#printing elements in reverse order using for loop
 for x in range(20,10,-1):
+    print(x)
+
+#printing even numbers from 0-100
+for x in range(0,101,2):
+    print(x)
+   
+#printing odd numbers from 0-100
+for x in range(1,100,2):
+    print(x)
+    
+#printing elements of a list in reverse order
+a=[1,2,3]
+for x in a[::-1]:
+    print(x)
+
+#printing elements of a list in sequence    
+a=[1,'abc',2,'pqr']
+for x in a:
     print(x)
