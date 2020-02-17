04c8d804e9d2c001bd3c371c2e992cedbef80daa
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-02-17 10:26:00 +0530

    Update printing-patterns.py

--- printing-patterns.py
+++ printing-patterns.py
@@ -7,3 +7,23 @@ o/p: ####
      ####
      ####
      ####
+
+for x in range(4):
+    for j in range(x+1):
+        print('#', end='')
+    print()
+    
+o/p: #
+     ##
+     ###
+     ####
+    
+for x in range(4):
+    for j in range(4-x):
+        print('#', end='')
+    print()
+   
+o/p: ####
+     ###
+     ##
+     #
