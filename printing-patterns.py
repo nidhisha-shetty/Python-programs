5c7a49a774bb5c36af943f2f82c62a3613309a66
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-02-18 10:14:43 +0530

    Update printing-patterns.py

--- printing-patterns.py
+++ printing-patterns.py
@@ -27,3 +27,23 @@ o/p: ####
      ###
      ##
      #
+
+for out in range(4):
+    for inn in range(0, out+1):
+        print(inn+1,end="")
+    print()
+   
+o/p:    1
+        12
+        123
+        1234
+        
+for x in range(4):
+    for y in range(x,4):
+        print(y+1,end="")
+    print() 
+    
+o/p:    1234
+        234
+        34
+        4   
