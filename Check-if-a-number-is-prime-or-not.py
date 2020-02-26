5e02a3041e4ed207a2971718637dcf8160326a16
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-02-26 23:20:51 +0530

    Create Check-if-a-number-is-prime-or-not.py

---
+++ Check-if-a-number-is-prime-or-not.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number "))
+for x in range(2,num):
+    if num%x==0:
+        print("The number is not prime")
+        break
+        
+else:
+    print("The number is prime")
