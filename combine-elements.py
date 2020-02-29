218dffe52c634e371cbe7a283716b9cb1f73c582
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-01 00:49:27 +0530

    Add files via upload

---
+++ combine-elements.py
@@ -0,0 +1,22 @@
+def combine_ele(li1, li2):
+    range_value=[]
+    range_value+=str(len(li1))
+    range_value+=str(len(li2))
+    range_value.sort()
+    res=[]
+    for x in range(int(range_value[-1])):
+        if x in range(len(li1)):
+            res+=str(li1[x])
+        if x in range(len(li2)):
+            res+=str(li2[x])
+    
+    
+    # res=[str(a) + str(b) for a, b in zip(li1, li2)]
+    #res=list(zip(li1, li2))
+    print(res)
+
+li1=['a','b','c','d','e']
+li2=[4,5,6]
+combine_ele(li1, li2)
+
+
