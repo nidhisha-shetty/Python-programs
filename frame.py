079d57961cd2b66258eb5fc861f5a2d8d7689822
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-03 23:29:06 +0530

    Update frame.py

--- frame.py
+++ frame.py
@@ -1,12 +1,11 @@
+#Function that takes a list of strings and prints them, one per line, in a rectangular frame.
+
 def frame(li1):
     print("**********")
     for x in li1:
-        
         print("*"+x,end=''+"*")
         print()
-    
     print("**********")
-    
-    
+     
 li1=list(input("Enter a list of strings").split(","))
 frame(li1)
