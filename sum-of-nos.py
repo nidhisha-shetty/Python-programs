30e7be196c0c2d888c3bcd2bb15b02523383f2c7
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-05 23:56:41 +0530

    Add files via upload

---
+++ sum-of-nos.py
@@ -0,0 +1,14 @@
+# x=[1,2,3]
+# sum=0
+# for z in x:
+#     sum+=z
+# print(sum)
+
+# x=[1,2,3]
+# sum=0
+# loop=0
+# print(len(x))
+# while (loop < len(x)):
+#     sum+=x[loop]
+#     loop+=1
+# print(sum)
