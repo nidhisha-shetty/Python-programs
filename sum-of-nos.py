233932a937a9edaa47789acc7ca79b22dfebc2b8
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-06 00:48:14 +0530

    Update sum-of-nos.py

--- sum-of-nos.py
+++ sum-of-nos.py
@@ -1,4 +1,4 @@
-#Find the sum of numbers using for loop
+#Find the sum of numbers in a list using for loop
 x=[1,2,3]
 sum=0
 for z in x:
@@ -6,7 +6,7 @@ for z in x:
 print(sum)
 
 
-#Find the sum of numbers using for while loop
+#Find the sum of numbers in a list using for while loop
 x=[1,2,3]
 sum=0
 loop=0
@@ -16,3 +16,15 @@ while (loop < len(x)):
     loop+=1
 print(sum)
 
+
+#Find the sum of numbers in a list using recurssion
+def sum(x):
+    if len(x)==1:
+        return x[0]
+    else:
+        return x[0]+sum(x[1:])
+    
+
+x=[1,2,3,4,5]
+res=sum(x)
+print(res)
