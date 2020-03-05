38ddb99821fa18834babe02ac7e6507eb6202982
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-05 23:57:56 +0530

    Update sum-of-nos.py

--- sum-of-nos.py
+++ sum-of-nos.py
@@ -1,14 +1,18 @@
-# x=[1,2,3]
-# sum=0
-# for z in x:
-#     sum+=z
-# print(sum)
+#Find the sum of numbers using for loop
+x=[1,2,3]
+sum=0
+for z in x:
+    sum+=z
+print(sum)
 
-# x=[1,2,3]
-# sum=0
-# loop=0
-# print(len(x))
-# while (loop < len(x)):
-#     sum+=x[loop]
-#     loop+=1
-# print(sum)
+
+#Find the sum of numbers using for while loop
+x=[1,2,3]
+sum=0
+loop=0
+print(len(x))
+while (loop < len(x)):
+    sum+=x[loop]
+    loop+=1
+print(sum)
+
