d2b87115a9ed7ad7a06cc8833d166d1b0a5bd7c3
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-10 23:32:59 +0530

    Add files via upload

---
+++ employee_change_status_program.py
@@ -0,0 +1,37 @@
+import sys
+
+class emplstatus:
+  count=0
+	emplist={"4":"active","5":"active"}
+
+	def __init__(self, eid, empstatus):
+		self.eid=eid
+		self.empstatus=empstatus
+
+	def changestatus(self, eid, newstatus):
+		self.eid=eid
+		self.newstatus=newstatus
+
+e=emplstatus(input("Enter new employee's id: "), input ("Enter new employee's status: "))
+
+if e.empstatus!="active":
+	print("The default status has to be active")
+	sys.exit()
+
+emplstatus.emplist[e.eid]=""+e.empstatus
+print(emplstatus.emplist)
+
+e.changestatus(input("Enter the employee's id for status change: "), input ("Enter the updated status: "))
+
+if e.eid in emplstatus.emplist:
+	emplstatus.emplist[e.eid]=""+e.newstatus
+else:
+	print("Invalid ID")
+
+print(emplstatus.emplist)
+
+emplstatus.count=emplstatus.count+sum(values == "active" for values in emplstatus.emplist.values())
+
+print("Total active employees are: "+str(emplstatus.count))
+
+
