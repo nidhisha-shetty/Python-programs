5a6d802d88167dd62cb0b26a7c84b49cdbfe44f8
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-04-05 23:18:16 +0530

    Modules in python

---
+++ adv_calc.py
@@ -0,0 +1,20 @@
+from calc import *           #or "import calc"
+
+x=7
+y=9
+
+def mul(a,b):
+	return a*b
+
+def div(a,b):
+	return a/b
+
+res1= add(x,y)				#if "import calc" on line 1 then "calc.add(x,y)"  
+res2= sub(x,y)				#if "import calc" on line 1 then "calc.sub(x,y)"
+res3= mul(x,y)			 	
+res4= div(x,y)				
+
+print(res1)
+print(res2)
+print(res3)			
+print(res4)
---
+++ calc.py
@@ -0,0 +1,14 @@
+a=5
+b=3
+
+def add(a,b):
+	return a+b
+
+def sub(a,b):
+	return a-b
+
+res1=add(a,b)
+res2=sub(a,b)
+
+print(res1)
+print(res2)
