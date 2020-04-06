66fd49b5d30786f0d47490207e24077eb7f490fe
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-04-06 11:02:36 +0530

    special variable __init__

---
+++ special_var.py
@@ -0,0 +1,12 @@
+def MH():
+	print("Maharashtra")
+def DL():
+	print("Delhi")
+
+def main():
+	MH()
+	DL()
+
+if __name__=="__main__":
+	main()
+
---
+++ special_var_1.py
@@ -0,0 +1,31 @@
+from special_var import *
+def CN():
+	print("Chennai")
+def WB():
+	print("West Bengal")
+
+MH()
+DL()
+CN()
+WB()
+
+#"from special_var import *" bydefault executes all the function in "special_var" program, hence the result of this program would be:
+
+# Maharashtra
+# Delhi
+# Maharashtra
+# Delhi
+# Chennai
+# West Bengal
+
+
+
+#Inorder to avoid this, we use "__name__" variable, which holds the value "__main__" for the module which is in execution. For the module which
+#is imported as a library, the __name__ variable will have **module name** as the value.
+
+#Add the following code in "special_var.py" program:
+# if __name__=="__main__":
+# 	main()
+
+
+
