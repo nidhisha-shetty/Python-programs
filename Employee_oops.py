e641820666763f96196fad1f75f9a1f702529a66
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-04-10 23:58:14 +0530

    Add files via upload

---
+++ Employee_oops.py
@@ -0,0 +1,16 @@
+class Employee:
+    hike=0.50										# class variable/static variable
+
+    def __init__(self, current_salary):
+        self.cur_sal=current_salary 				# instance variable
+
+    def revised_sal(self):
+        increament=self.cur_sal*Employee.hike
+        revised_sal=increament+self.cur_sal
+        print(revised_sal)
+
+e1=Employee(500)
+e2=Employee(1000)
+
+e1.revised_sal()
+e2.revised_sal()
