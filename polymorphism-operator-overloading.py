7f4c984ddd69471894870929a92d1262e0b8f50a
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-04-20 00:20:30 +0530

    Types of Polymorphism - Operator overloading

--- polymorphism-operator-overloading.py
+++ polymorphism-operator-overloading.py
@@ -1,26 +1,7 @@
-# a=12
-# b=6
 
-# print(a+b)
-# print(int.__add__(a,b)) #print(a+b), all the operators(+,*,/,-) behind the scene works as methods (__add__(),----) int is a class with __add__ method
-# print(a*b)
-# print(int.__mul__(a,b))
-# print(a/b)
-# print(int.__div__(a,b))
-# print(a-b)
-# print(int.__sub__(a,b))
-
-# #print(a+b) calls print(int.__add__(a,b))
-# #print(a*b) calls print(int.__mul__(a,b))
-# #print(a/b) calls print(int.__div__(a,b))
-# #print(a-b) calls print(int.__sub__(a,b))
-
-
-
-
-# overloading an binary +,-,* operator 
-# built-in method for addition operator(+) supports addition of integers, combining two strings, however does not support addition of two objects, 
-#we can define addition of two objects using operator oerloading
+# overloading an binary operator (+,-,*)
+# built-in method for addition operator(+) supports addition of integers, combining two strings, however does not support addition of two class objects, 
+#we can define addition of two class objects using operator overloading (changing the type of arguments/operands of the operator)
 
 class Students: 
 
@@ -28,7 +9,7 @@ class Students:
         self.mark = marks
 
     # adding two objects 
-    def __add__(a, b): 
+    def __add__(stud1, stud2): 
         return stud1.mark + stud2.mark
 
     # multiplying two objects
@@ -47,24 +28,19 @@ print(stud1 * stud2)
 print(stud1 - stud2)
 
 
-
-##################
-
-#Same above program with different argument names
+#Same above program with different arguments 
 
 class Students: 
     def __init__(self, marks): 
         self.mark = marks
 
     # adding two objects 
-    def __add__(a, b): 
+    def __add__(a, b):             #a.mark=stud1.mark & b.mark=stud2.mark
         return a.mark + b.mark
-        
+    # multiplying two objects
     def __mul__(a, b):
         return a.mark * b.mark
 
-    
-        
 stud1 = Students(12) 
 stud2 = Students(6) 
 
