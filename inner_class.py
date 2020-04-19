15547858ccd5934f53155f185578bff6a2edfe0e
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-04-19 23:55:16 +0530

    Inner/nested classes

--- inner_class.py
+++ inner_class.py
@@ -1,21 +1,27 @@
+#Inorder to avoid importing class (if it is used for one other class) we can use inner/nested class
 class Human:
+	
 	def __init__(self):
 		self.innerclass_nose=self.innerclass_nose()
 		self.innerclass_eyes=self.innerclass_eyes()
 		self.innerclass_brain=self.innerclass_brain()
 		print("This is the human body")
-
-
 		
 	class innerclass_nose:
+		
 		def __init__(self):
-			print("The nose is the body's primary organ of smell and also functions as part of the body's respiratory system.")
+			print("Nose class.")
+			
 	class innerclass_eyes:
+		
 		def __init__(self):
-			print("Eyes are organs of the visual system.")
+			print("Eyes class.")
+			
 	class innerclass_brain:
+		
 		def __init__(self):
-			print("Brain")
+			print("Brain class")
+			
 		def func(self):
 			print("The human brain is the command center for the human nervous system.")
 
