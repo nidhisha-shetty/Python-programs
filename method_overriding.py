3d7bdbd73cb1b49fae413f44c36ce06d88b399ba
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-04-20 23:59:13 +0530

    Update method_overriding.py

--- method_overriding.py
+++ method_overriding.py
@@ -1,18 +1,28 @@
+#Example 1, with output "Method A"
+
 class A:
+	
 	def show(self):
 		print("Method A")
+		
 class B(A):
+	
 	pass
+
 b1=B()
 b1.show()
 
 
+#Example 2, with output "Method B
+class A:
 
-# class A:
-# 	def show(self):
-# 		print("Method A")
-# class B(A):
-# 	def show(self):     	 #method overriding (show() of class B overrides show() method of A)
-# 		print("Method B")
-# a1=B()
-# a1.show()
+	def show(self):
+		print("Method A")
+
+class B(A):
+
+	def show(self):     		 #method overriding (show() of class B overrides show() method of class A)
+		print("Method B")
+
+a1=B()
+a1.show()
