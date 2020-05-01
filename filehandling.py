09fa6bdef1375b9b3ce7ee8c5a5eb65171cbf9a6
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-05-02 00:18:58 +0530

    FileHandling/Accessing in Python

--- filehandling.py
+++ filehandling.py
@@ -1,19 +1,16 @@
 	#Reading a file
-# file=open("filehandling.txt", 'r')
-# print(file.readline(4))
+file=open("filehandling.txt", 'r')	#opens a file and reads content
+print(file.readline(4))
 
 
 	#Writing a file
-# file=open("file",'w')		#creates file
-# file.write("first line")	#adds this line
+file=open("file",'w')			#opens a file and writes content (creates file if file doesnot exists)
+file.write("first line")		#adds this line
 
 
 	#Copying content from one file to another
-filehandling=open("filehandling.txt",'r')
-filehandlingcopy=open("filehandlingcopy",'w')
+filehandling=open("filehandling.txt",'r')		#opens a file and reads content
+filehandlingcopy=open("filehandlingcopy",'w')		#opens a file and writes content (creates file if file doesnot exists)
 
-for data in filehandling:
+for data in filehandling:				#using for-loop to copy content from 'filehandling' file to 'filehandlingcopy' file file
 	filehandlingcopy.write(data)
-
-
-
