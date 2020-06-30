cb277c9d3e6a209f45cfda3d62dcdf017ee354b8
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-07-01 00:29:00 +0530

    Add 'remove characters' program

---
+++ python practice programs/remove_characters.py
@@ -0,0 +1,11 @@
+'''
+#Given a string and an integer number n, remove characters from a string starting from zero up to n and return a new string
+
+def remove_chars(var):
+    i=int(input("enter a number"))
+    print(var[i:len(var)+1])
+var=input("Enter a word")
+remove_chars(var)
+
+'''
+
