b6313551e2dbbc89757ec5e3612c936dc32437b7
Nidhisha Shetty <shetty3491@gmail.com>
2020-07-19 00:07:36 +0530

    Update basics and data-structures.py

--- basics and data-structures.py
+++ basics and data-structures.py
@@ -32,7 +32,7 @@ li_num[4] or li_num[-2]
 #retrieving all the elements from the list
 lin_num[:]  
 
-#retrieving all the elements in reverse order from the list
+#retrieving all the elements in order from the list
 li_num[::-1]
 
 #list of string type
@@ -48,17 +48,27 @@ data=[li_num, li_str]
 li_data=['python',2.5,3]  #list containing a string, integer, and a float element
 
 #list is mutable i.e it can be changed using following methods
-li_data.append('Java')        #Java will be added as the last element of the list
+li_data.append('Java')        #Java will be added as the last element of the list; ['python', 2.5, 3, 'Java']
 
-li_data.insert(1, 'IDLE')     #'IDLE' will be added at index number 1
+li_data.insert(1, 'IDLE')     #'IDLE' will be added at index number 1; ['python', 'IDLE', 2.5, 3, 'Java']
 
-li_data.remove(3)             #element 3 will be removed from the list, remove() takes the element as the argument and not the index number, to remove element using index number try pop()
+li_data.remove(3)             #element 3 will be removed from the list, remove() takes the element to be removed as the argument and not the index number, to remove element using index number use pop() method; ['python', 'IDLE', 2.5, 'Java']
 
-li_data.pop(0)                #pop() removes the elemenet using index number, element at index number 0 is removed
+li_data.pop(0)                #pop() removes the elemenet using index number, element at index number 0 is removed; ['IDLE', 2.5, 'Java']
 
-li_data.pop()                 #removes the last element from the list
+li_data.pop()                 #removes the last element from the list; ['IDLE', 2.5]
 
-del li_data[1:]               # deletes all element from index number 1
+li_data.reverse()             #reverses the list; [2.5, 'IDLE']
+
+li_data.index('IDLE')         #returns the index number of the element passed as argument; if the element occurs twice in the list, first index number of the element is returned
+
+li_data.count(2.5)            #returns the count of occurence of the element in the list
+
+li_data.copy()                #returns a copy of the list(li_data); [2.5, 'IDLE']
+
+li_data.clear()               #removes all element from the list; []
+
+del li_data[1:]               #deletes all element from index number 1
 
 li_data.extend(['xyz', 34, 56, 'mno'])   #adding multiple elements to the list
 
@@ -72,7 +82,7 @@ li_data.sort()  #sorts the list in ascending order
 
                                              
   
-                                              #tuples
+                                              #Tuples
 #tuples are immutable, unlike list, used when the elements are fixed and will not be changed. Since they are immutable the speed of execution is faster than the list
 tu_data=(1,2,3,4,4,'abc')
 
