b43715b2ca5b71e27c8d4df1a9b45d7436a4f1dc
Nidhisha Shetty <shetty3491@gmail.com>
2020-07-19 23:18:49 +0530

    Add 'downward half pyramid.py' file

---
+++ python practice programs/downward-half-pyramid.py
@@ -0,0 +1,13 @@
+#method 1
+z=5
+for x in range(z):
+    for y in range(z,0,-1):
+        print('*', end="")
+    z=z-1
+    print('\n')
+
+#method 2
+for x in range(6,0,-1):
+    for y in range(x-1,0,-1):
+        print('*',end=' ')
+    print('\n')
