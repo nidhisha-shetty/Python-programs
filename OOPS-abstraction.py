69466631708ba1d4c33115c7fef5e0e1205ed41b
Nidhisha Shetty <shetty3491@gmail.com>
2020-09-24 08:33:28 +0530

    Update OOPS-abstraction.py

--- OOPS-abstraction.py
+++ OOPS-abstraction.py
@@ -1,4 +1,4 @@
-#abstract classes can be considered as blue print for other classes. abstract classes have methods which are just declared and not defined
+#abstract classes can be considered as blue print for other classes. abstract classes have methods which are just defined and not declared
 #abstract classes are used when you dont want to create object of a particular class, for example, great apes(having feature of moving), there cannot be direct objects for 'great apes', however we can have 'humans' as a class inherited from 'great apes' and have the definition of moving within 'human' class, also we can have 'lion' as a class inherited from 'great apes' and have the definition of moving within the 'lion' class
 
 #Since python doesnot have built-in abstract feature, so to define a class as abstract import 'ABC' class from 'abc' package (from abc import ABC, abstractmethod), to make a method 'abstract' add a decorator(@abstractmethod) before the method within abstract class
@@ -9,15 +9,15 @@ class Flower(ABC):
     def petals(self):
         pass
 
-class Rose:
+class Rose(Flower):
     def petals(self):
         print("Rose has many petals and are red in color")
 
-class Sunflower:
+class Sunflower(Flower):
     def petals(self):
         print("Sunflower has five petals and are yellow in color")
 
-class Shoeflower:
+class Shoeflower(Flower):
     def petals(self):
         print("Shoeflower has six petal and are pink in color")
 
