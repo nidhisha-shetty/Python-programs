f3494e8aa86f2c9a7eefb0b5f3ee8f0253fcdf1e
Nidhisha Shetty <shetty3491@gmail.com>
2020-10-28 23:36:02 +0530

    Find min value and max value usng single for loop

--- arrays/find-missing-element.py
+++ arrays/find-missing-element.py
@@ -12,12 +12,11 @@ missingelems(li)
 def missingelems(li):
     miss=[]
     min_val = max_val=li[0]
-    for min_x in li:
-        if min_x < min_val:
-            min_val=min_x
-    for max_x in li:
-        if max_x > max_val:
-            max_val = max_x
+    for x in li:
+        if x < min_val:
+            min_val=x
+        if x > max_val:
+            max_val = x
     for y in range(min_val, max_val):
         if y not in li:
             miss.append(y)
