fc65ef54eab38b8388bcf6d387e33371026eaf98
Nidhisha Shetty <shetty3491@gmail.com>
2020-10-31 19:06:14 +0530

    Update find-duplicates.py

--- arrays/find-duplicates.py
+++ arrays/find-duplicates.py
@@ -1,5 +1,4 @@
 #Find duplicates in an array
-
 li=[2,2,1,5,4,5,7,5]
 dup=[]
 for x in li:
@@ -7,3 +6,12 @@ for x in li:
     if res>1:
         dup.append(x)
 print(set(dup))
+
+#Removing duplicates
+def remove_duplicate(li):
+    for x in range(len(li)-1):
+        if li[x]==li[x+1]:
+            li.remove(li[x+1])
+    return li
+li=[1,1,2,2,3,4,5]
+print(remove_duplicate(li))
