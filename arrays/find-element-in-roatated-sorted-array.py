e3f7c3080e6549b622e2a74398d16891b4927d0e
Nidhisha Shetty <shetty3491@gmail.com>
2020-11-10 01:05:11 +0530

    Update find-element-in-roatated-sorted-array.py

--- arrays/find-element-in-roatated-sorted-array.py
+++ arrays/find-element-in-roatated-sorted-array.py
@@ -1,1 +1,17 @@
+# 7. How do you search a target value in a rotated array?
 
+arr=[15,18,2,5,6,8]
+target=8
+min=0
+for x in range(len(arr)):
+    if arr[x]<arr[min]:
+        min=x                  
+def bs(l,r):
+    mid=(l+r)//2
+    if arr[mid]==target:
+        return mid
+    if arr[0]<target:
+        bs(0,mid-1)
+    if arr[0]>target:
+        bs(mid+1,len(arr)-1)
+bs(0,len(arr)-1)    
