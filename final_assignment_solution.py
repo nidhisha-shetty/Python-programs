64391e1019d1d37e50d05c8c06b0d445742e6380
Nidhisha Shetty <shetty3491@gmail.com>
2021-02-23 00:01:48 +0530

    Update final_assignment_solution.py

--- final_assignment_solution.py
+++ final_assignment_solution.py
@@ -1,35 +1,11 @@
-# def get_all_values(nested_dictionary, inp):
-#     for key, value in nested_dictionary.items():
-#         if key==inp:
-#             print(value)       
-#         elif type(value) is dict:
-#             get_all_values(value, inp)
-
-# nested_dictionary = {
-#     "key1": "value1",
-#     "key2": "value2",
-#     "key3": {
-#         "key4" : "value4",
-#         "key5" : "value5",
-#         "key6" :{
-#             "key7" : "value7",
-#             "key8" : "value8"
-#         }
-#     }
-# }
-# inp=input("Enter a key")
-# get_all_values(nested_dictionary, inp)
-
-
+#Fetch the value of nested dictionary
 
 def get_all_values(nested_dictionary, inp):
     for key, value in nested_dictionary.items():
         if key==inp:
-            print(value)       
+            print(value)
         elif type(value) is dict:
-            for key, value in nested_dictionary.items():
-                if key==inp:
-                    print(value)
+            get_all_values(value, inp)
 
 nested_dictionary = {
     "key1": "value1",
