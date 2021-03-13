e76443fb17daacb3130001cf386c0ca75df5df9e
Nidhisha Shetty <shetty3491@gmail.com>
2021-03-13 22:09:48 +0530

    Update fetch_value_from_nested_dictionary.py

--- fetch_value_from_nested_dictionary.py
+++ fetch_value_from_nested_dictionary.py
@@ -1,4 +1,4 @@
-#Fetch the value of nested dictionary
+#Fetch the value of nested dictionary using recurssion
 
 def get_all_values(nested_dictionary, inp):
     for key, value in nested_dictionary.items():
@@ -21,3 +21,31 @@ nested_dictionary = {
 }
 inp=input("Enter a key")
 get_all_values(nested_dictionary, inp)
+
+#---------------------------------------------------
+
+#Fetch the value of nested dictionary using for loop
+
+def dictionary_path(res):
+  path=dict
+  for key in res:
+    if key in path:
+      path=path[key]
+    else:
+      return False
+dict = {
+  'yo': "man",
+  'hello': "hey",
+  'jinga': {
+    'key_one': "value",
+    'another': {
+      'key_two': "value",
+      'ano': {
+        'key_three': "value"
+      }
+    }
+  }
+}
+inp="jinga.another.ano.key_three"
+res=inp.split('.')
+print(dictionary_path(res))
