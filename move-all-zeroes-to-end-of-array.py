a13ed12a969a0a6ee0f6fa65685f3c849b073353
Nidhisha Shetty <shetty3491@gmail.com>
2021-05-04 17:24:17 +0530

    Update move-all-zeroes-to-end-of-array.py

--- move-all-zeroes-to-end-of-array.py
+++ move-all-zeroes-to-end-of-array.py
@@ -1,14 +1,15 @@
 # Given an array of random numbers, Push all the zero’s of a given array to the end of the array. 
 
 #Solution:
-inp=[1, 2, 0, 0, 0, 3, 6]
-count=0
-for x in range(len(inp)):
-    if inp[x]!=0:
-        inp[x],inp[count]=inp[count],inp[x]
-        count+=1
+def push_zero_to_end(inp):
+    count=0
+    for x in range(len(inp)):
+        if inp[x]!=0:
+            inp[x],inp[count]=inp[count],inp[x]
+            count+=1
 
-print(inp)
+inp=[1, 2, 0, 0, 0, 3, 6]
+print(push_zero_to_end(inp))
 
 #Time complexity: O(n) 
 #Space Complexity: O(1)
