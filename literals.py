065add4fbcf0b6fa57fceed710df15656175ea9b
Nidhisha Shetty <shetty3491@gmail.com>
2022-01-13 23:08:18 +0530

    Create literals.py

---
+++ literals.py
@@ -0,0 +1,24 @@
+'''
+String literals: A string literal is created by a collection of characters surrounded by single ('), double("), triple single(''') or triple double (""") quotes.
+For example, 
+'''
+
+var1 = "This is a string in double quotes"
+var2 = 'This is a string in single quotes'
+var3 = '''This is a 
+string in triple single quotes'''
+var4 = """This is a 
+string in triple double quotes"""
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+
+#Output:
+This is a string in double quotes
+This is a string in single quotes
+This is a 
+string in triple single quotes
+This is a 
+string in triple double quotes
