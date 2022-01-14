ec0ed59c25a7022bbda87daebf67562993681f81
Nidhisha Shetty <shetty3491@gmail.com>
2022-01-14 23:13:04 +0530

    Update literals.py

--- literals.py
+++ literals.py
@@ -22,3 +22,24 @@ This is a
 string in triple single quotes
 This is a 
 string in triple double quotes
+
+'''
+Numeric literals: Numeric literals can not be changed, that is, it is immutable. It includes Float, Complex, and Integer literals.
+
+Float contain the fractional parts, for example, e = 24.8
+Complex numbers contain real and imaginary numbers in the form of a+bj, for example, z = 7 + 5j
+Integer contains both positive and negative values but not fractions. Following are types of Integer literal:
+
+Decimal literal:
+A decimal literal contains digits from 0 to 9. For example: 
+b = 50
+
+Binary literal:
+A binary literal contains digits 0’s and 1’s with prefix 0b or 0B.
+
+Octal literal:
+Octal literal contains 0 at the start of the number.
+
+Hexadecimal literal:
+A hexadecimal number has 16 digits, that is, 0-9 and A-F, representing numbers from 10 - 15. It has 0x prefix or h postfix to the number.
+'''
