3accff4e632341270796d141f31b34c3eb25a183
Nidhisha Shetty <shetty3491@gmail.com>
2022-05-27 07:57:23 +0530

    Create oops-method-overriding-example.py

---
+++ oops-method-overriding-example.py
@@ -0,0 +1,30 @@
+'''
+P.S: Implement method overriding, if the numbers are greater than 100 then call calculate() of child class else call calculate() of parent class.
+'''
+
+#Solution:
+class Parent():
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    def calculate(self):
+        res = self.num1 + self.num2
+        return res
+
+class Child(Parent):
+    def __init__(self, num1, num2):
+        super().__init__(num1, num2)
+    
+    def calculate(self):
+        if self.num1 < 100 and self.num2 <100:
+            return super().calculate()
+        else:
+            res = self.num1 * self.num2
+            return res
+
+num1 = 30
+num2 = 60
+
+c1 = Child(num1, num2)
+print(c1.calculate())
