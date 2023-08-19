61d3fbef34a52bb0ba22094c1b09a6ed5824956d
Nidhisha Shetty <shetty3491@gmail.com>
2023-08-19 19:18:30 +0530

    Update python-redis-cache.py

--- python-redis-cache.py
+++ python-redis-cache.py
@@ -4,3 +4,13 @@ r.set('France', 'Paris') #setting redis key('France') and value('Paris')
 r.set('India', 'Delhi')
 india_capital = r.get('India') #getting value of a redis key
 print(india_capital) 
+
+'''
+Steps to install redis
+1. pip install redis
+2. sudo apt-get install redis-server
+Start redis
+1. on terminal run 'redis-cli' command 
+2. run 'keys *' command to get all keys
+3. run 'GET <key-name>' command to get the value of key
+'''
